2b8d25ff7eb8d6f16ebfe14ae4230e4844a38a1d
jkcoderdev <20jackoza06@gmail.com>
2024-11-11 18:47:10 +0100

    Add loading screen

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import { FlatContext } from './render/2d/FlatContext.js';
 
 import { AssetsManager } from './assets/AssetsManager.js';
 
+const loading = document.querySelector('#loading');
+
 const assets = new AssetsManager();
 assets.addImage('landscape', 'minekhan-landscape.png');
 
@@ -11,6 +13,10 @@ const overlayContext = new FlatContext('canvas#overlay');
 
 (async function() {
     const { fonts, images, shaders, workers } = await assets.loadEverything();
+
+    // All assets are loaded so remove loading screen
+    const loadingDisapearAnimation = loading.animate({ opacity: 0 }, { duration: 500 });
+    loadingDisapearAnimation.addEventListener('finish', () => loading.remove());
 
     console.log(fonts);
     console.log(images),
